988c17797d5aef28c867b80e31c7fd4cbf5973d3
Duarte Duarte <duartenuno1992@hotmail.com>
2015-04-10 13:26:46 +0100

    [Add] database

--- Geocaching/src/main/java/base/Position.java
+++ Geocaching/src/main/java/base/Position.java
@@ -1,5 +1,7 @@
 package base;
 
+import java.util.GregorianCalendar;
+
 public class Position {
 	private double lati = 0.0;
 	private double longi = 0.0;
@@ -7,58 +9,98 @@ public class Position {
 	private String city = "";
 	private float difficulty = 0;
 
-    public Position(double lati, double longi) {
-        this.lati = lati;
-        this.longi = longi;
-    }
+	public Position(double lati, double longi) {
+		this.lati = lati;
+		this.longi = longi;
+	}
 
-    public Position(double lati, double longi, String country, String city, float difficulty) {
-        this.lati = lati;
-        this.longi = longi;
-        this.country = country;
-        this.city = city;
-        this.difficulty = difficulty;
-    }
+	public Position(double lati, double longi, String country, String city,
+			float difficulty) {
+		this.lati = lati;
+		this.longi = longi;
+		this.country = country;
+		this.city = city;
+		this.difficulty = difficulty;
+	}
 
-    public double getLongi() {
-        return longi;
-    }
+	public double getLongi() {
+		return longi;
+	}
 
-    public void setLongi(double longi) {
-        this.longi = longi;
-    }
+	public void setLongi(double longi) {
+		this.longi = longi;
+	}
 
-    public double getLati() {
-        return lati;
-    }
+	public double getLati() {
+		return lati;
+	}
 
-    public void setLati(double lati) {
-        this.lati = lati;
-    }
+	public void setLati(double lati) {
+		this.lati = lati;
+	}
 
-    public String getCountry() {
-        return country;
-    }
+	public String getCountry() {
+		return country;
+	}
 
-    public void setCountry(String country) {
-        this.country = country;
-    }
+	public void setCountry(String country) {
+		this.country = country;
+	}
 
-    public String getCity() {
-        return city;
-    }
+	public String getCity() {
+		return city;
+	}
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+	public void setCity(String city) {
+		this.city = city;
+	}
 
-    public float getDifficulty() {
-        return difficulty;
-    }
+	public float getDifficulty() {
+		return difficulty;
+	}
 
-    public void setDifficulty(float difficulty) {
-        this.difficulty = difficulty;
-    }
+	public void setDifficulty(float difficulty) {
+		this.difficulty = difficulty;
+	}
 
+	public int getQuadrant() {
+		int res = 0;
+		if (this.longi >= 0 && this.lati <= 0) {
+			res = 1;
+		} else if (this.longi >= 0 && this.lati > 0) {
+			res = 2;
+		} else if (this.longi < 0 && this.lati <= 0) {
+			res = 3;
+		} else
+			res = 4;
 
+		return res;
+	}
+
+	public int getSeason(boolean northHemisphere, GregorianCalendar date) {// 0-winter;
+																			// 1-spring;
+		// 2-summer; 3-autumn
+		int res = -1;
+		int weeks = date.getWeekYear();
+		if (northHemisphere) {
+			if (weeks < 13) {
+				res = 0;
+			} else if (weeks < 26) {
+				res = 1;
+			} else if (weeks < 39) {
+				res = 2;
+			} else
+				res = 3;
+		} else {
+			if (weeks < 13) {
+				res = 2;
+			} else if (weeks < 26) {
+				res = 3;
+			} else if (weeks < 39) {
+				res = 0;
+			} else
+				res = 1;
+		}
+		return res;
+	}
 }
--- Geocaching/src/main/java/meteo/Meteo.java
+++ Geocaching/src/main/java/meteo/Meteo.java
@@ -1,22 +1,31 @@
 package meteo;
 
+import java.util.GregorianCalendar;
 import java.util.Random;
+
+import base.Position;
 
 public class Meteo {
 
 	private float temperature;
 	private float rainProbability;
-    private String summary;
+	private String summary;
 
 	public Meteo() {
 		this.temperature = 15.0f;
 		this.rainProbability = 0.25f;
-    }
+	}
 
 	public Meteo(float temperature, float rainProbability, String summary) {
 		this.temperature = temperature;
 		this.rainProbability = rainProbability;
 		this.summary = summary;
+	}
+
+	public Meteo(float temperature, float rainProbability) {
+		this.temperature = temperature;
+		this.rainProbability = rainProbability;
+		this.summary = "Estimated";
 	}
 
 	public Meteo(Meteo m) {
@@ -25,30 +34,29 @@ public class Meteo {
 		this.summary = m.getSummary();
 	}
 
-    public float getTemperature() {
-        return temperature;
-    }
+	public float getTemperature() {
+		return temperature;
+	}
 
-    public void setTemperature(float temperature) {
-        this.temperature = temperature;
-    }
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
 
-    public float getRainProbability() {
-        return rainProbability;
-    }
+	public float getRainProbability() {
+		return rainProbability;
+	}
 
-    public void setRainProbability(float rainProbability) {
-        this.rainProbability = rainProbability;
-    }
+	public void setRainProbability(float rainProbability) {
+		this.rainProbability = rainProbability;
+	}
 
-    public String getSummary() {
-        return summary;
-    }
+	public String getSummary() {
+		return summary;
+	}
 
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
 
 	@Override
 	public boolean equals(Object o) {
@@ -71,8 +79,8 @@ public class Meteo {
 		sb.append(this.temperature);
 		sb.append("\nRain Probability: ");
 		sb.append(this.rainProbability);
-        sb.append("\nSummary: ");
-        sb.append(this.summary);
+		sb.append("\nSummary: ");
+		sb.append(this.summary);
 		return sb.toString();
 	}
 
@@ -85,14 +93,112 @@ public class Meteo {
 			return 1;
 	}
 
-    public Meteo autoMeteo(){
-        Random randomno = new Random();
-        float temp = randomno.nextFloat();
-        return new Meteo(temp*25, randomno.nextFloat(), "Estimated");
-    }
+	public Meteo autoMeteo() {
+		Random randomno = new Random();
+		float temp = randomno.nextFloat();
+		return new Meteo(temp * 25, randomno.nextFloat());
+	}
 
 	@Override
 	public Meteo clone() {
 		return new Meteo(this);
+	}
+
+	public Meteo autoMeteo(Position position, GregorianCalendar date) {
+		Meteo res = null;
+		int quadrant = position.getQuadrant();
+		boolean northHemisphere = quadrant % 2 != 0;
+		int season = position.getSeason(northHemisphere, date); // 0-winter;
+																// 1-spring;
+																// 2-summer;
+																// 3-autumn
+
+		Random randomno = new Random();
+		switch (quadrant) {
+		case 1:
+			switch (season) {
+			case 0:
+				res = new Meteo(randomno.nextFloat() * 10,
+						randomno.nextFloat() * 85);
+				break;
+			case 1:
+				res = new Meteo(randomno.nextFloat() * 25,
+						randomno.nextFloat() * 55);
+				break;
+			case 2:
+				res = new Meteo(randomno.nextFloat() * 40,
+						randomno.nextFloat() * 5);
+				break;
+			case 3:
+				res = new Meteo(randomno.nextFloat() * 25,
+						randomno.nextFloat() * 50);
+				break;
+			}
+			break;
+		case 2:
+			switch (season) {
+			case 0:
+				res = new Meteo(randomno.nextFloat() * 7,
+						randomno.nextFloat() * 90);
+				break;
+			case 1:
+				res = new Meteo(randomno.nextFloat() * 30,
+						randomno.nextFloat() * 45);
+				break;
+			case 2:
+				res = new Meteo(randomno.nextFloat() * 40,
+						randomno.nextFloat() * 15);
+				break;
+			case 3:
+				res = new Meteo(randomno.nextFloat() * 30,
+						randomno.nextFloat() * 40);
+				break;
+			}
+			break;
+		case 3:
+			switch (season) {
+			case 0:
+				res = new Meteo(randomno.nextFloat() * 30,
+						randomno.nextFloat() * 60);
+				break;
+			case 1:
+				res = new Meteo(randomno.nextFloat() * 35,
+						randomno.nextFloat() * 40);
+				break;
+			case 2:
+				res = new Meteo(randomno.nextFloat() * 45,
+						randomno.nextFloat() * 25);
+				break;
+			case 3:
+				res = new Meteo(randomno.nextFloat() * 35,
+						randomno.nextFloat() * 30);
+				break;
+			}
+			break;
+		case 4:
+			switch (season) {
+			case 0:
+				res = new Meteo(randomno.nextFloat() * 40,
+						randomno.nextFloat() * 10);
+				break;
+			case 1:
+				res = new Meteo(randomno.nextFloat() * 40,
+						randomno.nextFloat() * 13);
+				break;
+			case 2:
+				res = new Meteo(randomno.nextFloat() * 45,
+						randomno.nextFloat() * 10);
+				break;
+			case 3:
+				res = new Meteo(randomno.nextFloat() * 40,
+						randomno.nextFloat() * 12);
+				break;
+			}
+			break;
+		default:
+			res = new Meteo();
+			break;
+		}
+		return res;
 	}
 }
