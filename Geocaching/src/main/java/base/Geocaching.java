d68e75f77db2ea61683ec9ff6d3e49ce9caa1261
Fábio Gomes <mrfabiogomes@gmail.com>
2015-05-23 00:45:05 +0100

    [Add] Show Logo when Program starts [Update] Reorganized methods

--- Geocaching/src/main/java/base/Geocaching.java
+++ Geocaching/src/main/java/base/Geocaching.java
@@ -26,55 +26,31 @@ public class Geocaching {
 
     static Data data = null;
     static BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-    static UserAbstract userOnline;
+    static UserAbstract userOnline; // User online and using the System
 
     public static void main(String[] args) {
 
         data = getData();
 
         //populateData(data);
-        System.out.println("---------------------------------------------------");
-        System.out.println("---------------- Welcome to GeoPOO ----------------");
-        System.out.println("---------------------------------------------------");
+        showLogo();
 
+        System.out.println("----------------------------------------------");
+        System.out.println("------------- Welcome to GeoPOO --------------");
+        System.out.println("----------------------------------------------");
         mStart();
 
     }
 
-    public final static void clearConsole() {
-        try {
-            final String os = System.getProperty("os.name");
+    // ------------------- MAIN MENU ------------------
+    private static void mStart() {
 
-            if (os.contains("Windows")) {
-                Runtime.getRuntime().exec("cls");
-            } else {
-                Runtime.getRuntime().exec("clear");
-            }
-        } catch (final Exception e) {
-            //  Handle any exceptions.
-        }
-    }
-
-    // Load Data if it exists else create new one
-    public static Data getData() {
-
-        // Load Data
-        if (loadData()) {
-            return data;
-        } else {
-            return new Data();
-        }
-    }
-
-    private static void mProfile() {
         int choice = -1;
         while (choice == -1) {
-            System.out.println("####### My Profile #######\n");
-            System.out.println(userOnline.toStringTotal() + "\n");
-            System.out.println("-- [1] Edit Profile");
-            System.out.println("-- [2] Premium Membership");
+            System.out.println("-- [1] Login");
+            System.out.println("-- [2] Register");
             System.out.println("-----");
-            System.out.println("-- [0] Back");
+            System.out.println("-- [0] Exit");
             System.out.print("?> ");
             try {
                 choice = Integer.parseInt(input.readLine());
@@ -86,24 +62,93 @@ public class Geocaching {
             switch (choice) {
                 case 1:
                     clearConsole();
-                    mEditProfile();
-                    choice = -1;
-                    clearConsole();
+                    mLogin();
                     break;
                 case 2:
                     clearConsole();
-                    mPremium();
+                    if (mSignUp()) {
+                        System.out.println("User Successfully Registered!");
+                    } else {
+                        System.out.println("User Registration Failed!");
+                    }
                     choice = -1;
-                    clearConsole();
                     break;
                 case 0:
+                    mExit();
                     break;
                 default:
                     System.out.println("Error: Option not available");
                     choice = -1;
                     break;
             }
+
         }
+
+    }
+
+    private static void mMain() {
+        int choice = -1;
+
+        while (choice == -1) {
+            System.out.println("####### Main Menu #######\n");
+            System.out.println("-- [1] Profile");
+            System.out.println("-- [2] Caches");
+            System.out.println("-- [3] Friends");
+            System.out.println("-- [4] Statistics");
+            System.out.println("-----:");
+            System.out.println("-- [0] Exit");
+
+            System.out.print("?> ");
+            try {
+                choice = Integer.parseInt(input.readLine());
+            } catch (Exception ex) {
+                System.out.println("Error: Invalid Option");
+                choice = -1;
+            }
+
+            switch (choice) {
+                case 1:
+                    clearConsole();
+                    mProfile();
+                    choice = -1;
+                    clearConsole();
+                    break;
+                case 2:
+                    clearConsole();
+                    mCaches();
+                    choice = -1;
+                    clearConsole();
+                    break;
+                case 3:
+                    clearConsole();
+                    mFriends();
+                    choice = -1;
+                    clearConsole();
+                    break;
+                case 4:
+                    clearConsole();
+                    mStats();
+                    choice = -1;
+                    clearConsole();
+                    break;
+                case 0:
+                    clearConsole();
+                    mExit();
+                    break;
+                default:
+                    System.out.println("Error: Invalid Option");
+                    break;
+            }
+        }
+    }
+
+    private static void mExit() {
+        if (saveData()) {
+            System.out.println("Program exit successfully!");
+        } else {
+            System.out.println("Program couldn't save the Data");
+        }
+        System.exit(0);
     }
 
     private static void mLogin() {
@@ -297,14 +342,16 @@ public class Geocaching {
         return true;
     }
 
-    private static void mStart() {
-
+    // ------------------- PROFILE MENU ------------------
+    private static void mProfile() {
         int choice = -1;
         while (choice == -1) {
-            System.out.println("-- [1] Login");
-            System.out.println("-- [2] Register");
+            System.out.println("####### My Profile #######\n");
+            System.out.println(userOnline.toStringTotal() + "\n");
+            System.out.println("-- [1] Edit Profile");
+            System.out.println("-- [2] Premium Membership");
             System.out.println("-----");
-            System.out.println("-- [0] Exit");
+            System.out.println("-- [0] Back");
             System.out.print("?> ");
             try {
                 choice = Integer.parseInt(input.readLine());
@@ -316,123 +363,24 @@ public class Geocaching {
             switch (choice) {
                 case 1:
                     clearConsole();
-                    mLogin();
+                    mEditProfile();
+                    choice = -1;
+                    clearConsole();
                     break;
                 case 2:
                     clearConsole();
-                    if (mSignUp()) {
-                        System.out.println("User Successfully Registered!");
-                    } else {
-                        System.out.println("User Registration Failed!");
-                    }
+                    mPremium();
                     choice = -1;
+                    clearConsole();
                     break;
                 case 0:
-                    mExit();
                     break;
                 default:
                     System.out.println("Error: Option not available");
                     choice = -1;
                     break;
             }
-
         }
-
-    }
-
-    private static void mMain() {
-        int choice = -1;
-
-        while (choice == -1) {
-            System.out.println("####### Main Menu #######\n");
-            System.out.println("-- [1] Profile");
-            System.out.println("-- [2] Caches");
-            System.out.println("-- [3] Friends");
-            System.out.println("-- [4] Statistics");
-            System.out.println("-----:");
-            System.out.println("-- [0] Exit");
-
-            System.out.print("?> ");
-            try {
-                choice = Integer.parseInt(input.readLine());
-            } catch (Exception ex) {
-                System.out.println("Error: Invalid Option");
-                choice = -1;
-            }
-
-            switch (choice) {
-                case 1:
-                    clearConsole();
-                    mProfile();
-                    choice = -1;
-                    clearConsole();
-                    break;
-                case 2:
-                    clearConsole();
-                    mCaches();
-                    choice = -1;
-                    clearConsole();
-                    break;
-                case 3:
-                    clearConsole();
-                    mFriends();
-                    choice = -1;
-                    clearConsole();
-                    break;
-                case 4:
-                    clearConsole();
-                    mStats();
-                    choice = -1;
-                    clearConsole();
-                    break;
-                case 0:
-                    clearConsole();
-                    mExit();
-                    break;
-                default:
-                    System.out.println("Error: Invalid Option");
-                    break;
-            }
-        }
-    }
-
-    private static void mExit() {
-        if (saveData()) {
-            System.out.println("Program exit successfully!");
-        } else {
-            System.out.println("Program couldn't save the Data");
-        }
-        System.exit(0);
-    }
-
-    private static boolean loadData() {
-        FileInputStream fin = null;
-        try {
-            fin = new FileInputStream("data.poo");
-            ObjectInputStream ois = new ObjectInputStream(fin);
-            data = (Data) ois.readObject();
-            fin.close();
-        } catch (FileNotFoundException ex) {
-            return false;
-        } catch (IOException | ClassNotFoundException ex) {
-            return false;
-        }
-        return true;
-    }
-
-    private static boolean saveData() {
-        FileOutputStream fout = null;
-        try {
-            fout = new FileOutputStream("data.poo");
-            ObjectOutputStream oos = new ObjectOutputStream(fout);
-            oos.writeObject(data);
-            fout.close();
-        } catch (FileNotFoundException ex) {
-            return false;
-        } catch (Exception ex) {
-            return false;
-        }
-        return true;
     }
 
     private static void mFriends() {
@@ -529,6 +477,7 @@ public class Geocaching {
 
     }
 
+    // ------------------- FRIENDS MENU ------------------
     private static void mViewFriend(User friend) {
         int choice = -1;
         while (choice == -1) {
@@ -590,6 +539,81 @@ public class Geocaching {
         }
     }
 
+    private static void mViewActivities(User friend) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void mViewStatistics(User friend) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void mViewFoundCaches(User friend) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void mViewOwnedCaches(User friend) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    // ------------------- OTHER STUFF ------------------
+    // Clear Console
+    public final static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                Runtime.getRuntime().exec("cls");
+            } else {
+                Runtime.getRuntime().exec("clear");
+            }
+        } catch (final Exception e) {
+            //  Handle any exceptions.
+        }
+    }
+
+    // Load Data if it exists else create new one
+    public static Data getData() {
+
+        // Load Data
+        if (loadData()) {
+            return data;
+        } else {
+            return new Data();
+        }
+    }
+
+    // Load Data from File
+    private static boolean loadData() {
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream("data.poo");
+            ObjectInputStream ois = new ObjectInputStream(fin);
+            data = (Data) ois.readObject();
+            fin.close();
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (IOException | ClassNotFoundException ex) {
+            return false;
+        }
+        return true;
+    }
+
+    // Save Data to File
+    private static boolean saveData() {
+        FileOutputStream fout = null;
+        try {
+            fout = new FileOutputStream("data.poo");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(data);
+            fout.close();
+        } catch (FileNotFoundException ex) {
+            return false;
+        } catch (Exception ex) {
+            return false;
+        }
+        return true;
+    }
+
     // Pre-Made Dataset
     private static void populateData(Data data) {
 
@@ -607,20 +631,31 @@ public class Geocaching {
 
     }
 
-    private static void mViewActivities(User friend) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void mViewStatistics(User friend) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void mViewFoundCaches(User friend) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private static void mViewOwnedCaches(User friend) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    // Show GeoCaching Logo
+    private static void showLogo() {
+        System.out.println("..............................................");
+        System.out.println(".............00xxxxxxxxxxxxxx00...............");
+        System.out.println("..........0x@x00.........0xx@@@@@x00..........");
+        System.out.println("........0xx0..................0x@@@@@x0.......");
+        System.out.println(".......xx........0xx0.............0x@@@@x.....");
+        System.out.println(".....0x0..........@@x...............0x@@@x....");
+        System.out.println("....0x............0@@0................000.....");
+        System.out.println("...0@..............0@x........................");
+        System.out.println("...@0...............x@..........000xxxx@@@x...");
+        System.out.println("..x@.................xx..00xx@@@@@@@@@@@x@@0..");
+        System.out.println("..@@...............00x@@@x000............0@x..");
+        System.out.println(".0@x......00xxx@@xx00..@@0................@@..");
+        System.out.println("..@@...0@@@@@@x0........@@0...............@@0.");
+        System.out.println("..x@0...xxx00...........0@@..............0@@0.");
+        System.out.println("...@x....................@@x.............x@@..");
+        System.out.println("...0@x...................0@@0...........0@@x..");
+        System.out.println("....0@@0..................x@x..........0@@x...");
+        System.out.println("......0@x0.................0..........0@@x....");
+        System.out.println("........x@@x0.......................0x@@0.....");
+        System.out.println("..........0x@@xx0................00@@@0.......");
+        System.out.println("..............0xx@@xx000000000xx@@x00.........");
+        System.out.println(".................00000000000000000............");
+        System.out.println("..............................................");
     }
 
 }
